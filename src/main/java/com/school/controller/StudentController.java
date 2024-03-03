c1a09f8c1158b90ddd5027e03a020a09e9b83d3b
MUKUND NARAYAN <mukundnarayan87@gmail.com>
2024-03-03 11:25:12 +0530

    add a endpont to test whether my application is working fine or not

--- src/main/java/com/school/controller/StudentController.java
+++ src/main/java/com/school/controller/StudentController.java
@@ -1,10 +1,15 @@
 package com.school.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
-	
+	@GetMapping("/start")
+	public String test() {
+		return "Hello bachho, Kaise ho bete!!";
+	}
 }
