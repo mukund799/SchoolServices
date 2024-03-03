1bf32eac38024bd93ede9614008a1397b8639a01
MUKUND NARAYAN <mukundnarayan87@gmail.com>
2024-03-03 12:07:51 +0530

    entity and controller changed

--- src/main/java/com/school/controller/StudentController.java
+++ src/main/java/com/school/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.school.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,13 @@ public class StudentController {
 	@GetMapping("/start")
 	public String test() {
 		return "Hello bachho, Kaise ho bete!!";
+	}
+	
+	
+	// to add student details into the database
+	@PostMapping("/addDetails")
+	public String addDetails() {
+		
+		return "Student Details Addedd.";
 	}
 }
--- src/main/java/com/school/entity/StudentEntity.java
+++ src/main/java/com/school/entity/StudentEntity.java
@@ -1,5 +1,74 @@
 package com.school.entity;
 
-public class StudentEntity {
+import jakarta.persistence.Entity;
 
+@Entity
+public class StudentEntity {
+	private int id;
+	private String name;
+	private String fatherName;
+	private String motherName;
+	private String rollNo;
+	private String section;
+	private String className;
+
+	public StudentEntity() {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+	public StudentEntity(int id, String name, String fatherName, String motherName, String rollNo, String section,
+			String className) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.fatherName = fatherName;
+		this.motherName = motherName;
+		this.rollNo = rollNo;
+		this.section = section;
+		this.className = className;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	public String getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(String rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getSection() {
+		return section;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	
 }
